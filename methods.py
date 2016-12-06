5cd918d33cc6b293c581139af9b6e47b85b93183
Hryo224 <hampton.young@uky.edu>
2016-12-06 16:58:00 -0500

    Added params for each method

--- methods.py
+++ methods.py
@@ -4,75 +4,113 @@ methods = {
 
     "CALENDAR":{
         "function":"calendar",
-        "endpoint":DATA_URL + "/calendar.json" 
+        "endpoint":DATA_URL + "/calendar.json",
+        "params":["none"]
     },
     "SCOREBOARD":{
         "function":"scoreboard",
         "endpoint":DATA_URL + "/%s/scoreboard.json",
-        "params":"date"
+        "params":["date (format YYYYMMDD"]
     },
 
     "TEAMS":{
         "function":"teams",
         "endpoint":DATA_URL + "/%s/teams.json",
-        "params":"year"
+        "params":["year"]
     },
     "PLAYERS_BY_YEAR":{
         "function":"players",
         "endpoint":DATA_URL + "/%s/players.json",
-        "params":"year"
+        "params":["year"]
     },
     "COACHES_BY_YEAR": {
         "function":"coaches",
         "endpoint":DATA_URL+"/%s/coaches.json",
-        "params":"year"
+        "params":["year"]
     },
-    "CONFERENCE_STANDINGS":{
+    "CURRENT_CONFERENCE_STANDINGS":{
         "function":"conference_standings",
-        "endpoint":DATA_URL+"/current/standings_conference.json"
+        "endpoint":DATA_URL+"/current/standings_conference.json",
+        "params":["none"]
     },
-    "LEAGUE_DIVISION_STANDINGS":{
+    "CURRENT_DIVISION_STANDINGS":{
         "function":"division_standings",
-        "endpoint":DATA_URL+"/current/standings_division.json"
+        "endpoint":DATA_URL+"/current/standings_division.json",
+        "params":["none"]
     },
-    "LEAGUE_UNGROUPED_STANDINGS":{
+    "STANDINGS":{
         "function":"standings",
-        "endpoint":DATA_URL+"/current/standings_all.json"
+        "endpoint":DATA_URL+"/current/standings_all.json",
+        "params":["none"]
     },
-    "LEAGUE_MINI_STANDINGS":{
+    "MINI_STANDINGS":{
         "function":'mini_standings',
-        "endpoint":DATA_URL+"/current/standings_all_no_sort_keys.json"
+        "endpoint":DATA_URL+"/current/standings_all_no_sort_keys.json",
+        "params":["none"]
     },
-    "LEAGUE_TEAM_STATS_LEADERS":{
+    "TEAM_STATS_RANKINGS":{
         "function":"team_stats_leaders",
-        "endpoint":DATA_URL+"%s/team_stats_rankings.json"
+        "endpoint":DATA_URL+"%s/team_stats_rankings.json",
+        "params":["year"]
     },
-    "LEAGUE_LAST_FIVE_GAME_TEAM_STATS":{
+    "TEAM_STATS_LAST_FIVE_GAMES":{
         "function":"last_five_game_team_stats",
-        "endpoint":DATA_URL+"/%s/team_stats_last_five_games.json"
+        "endpoint":DATA_URL+"/%s/team_stats_last_five_games.json",
+        "params":["year"]
     },
     "PREVIEW_ARTICLE":{
         "function":"preview_article",
-        "endpoint": DATA_URL+"/%s/%s_preview_article.json"
+        "endpoint": DATA_URL+"/%s/%s_preview_article.json",
+        "params":["date (format YYYYMMDD","gameId"]
     },
     "TEAM_LEADERS":{
         "function":"team_leaders",
-        "endpoint":DATA_URL+"/%s/teams/%s/leaders.json"
+        "endpoint":DATA_URL+"/%s/teams/%s/leaders.json",
+        "params":["year","teamName"]
     },
     "RECAP_ARTICLE":{
         "function":"recap_article",
-        "endpoint":DATA_URL+"/%s/%s_recap_article.json"
+        "endpoint":DATA_URL+"/%s/%s_recap_article.json",
+        "params":["date (format YYYYMMDD", "gameId"]
     },
     "BOXSCORE":{
         "function":"boxscore",
-        "endpoint":DATA_URL+"/%s/%s_boxscore.json"
+        "endpoint":DATA_URL+"/%s/%s_boxscore.json",
+        "params":["date (format YYYYMMDD", "gameId"]
     },
     "PLAY_BY_PLAY":{
         "function":"play_by_play",
-        "endpoint":DATA_URL+"/%s/%s_pbp_%s.json"
+        "endpoint":DATA_URL+"/%s/%s_pbp_%s.json",
+        "params":["date (format YYYYMMDD", "gameId", "period"]
     },
     "LEADTRACKER":{
         "function":"leadtracker",
-        "endpoint":DATA_URL+"/%s/%s_lead_tracker_%s.json"
+        "endpoint":DATA_URL+"/%s/%s_lead_tracker_%s.json",
+        "params":["date (format YYYYMMDD", "gameId", "period"]
+    },
+    "MINI_BOXSCORE":{
+        "function":"mini_boxscore",
+        "endpoint":DATA_URL+"/%s/%s_mini_boxscore.json",
+        "params":["date (format YYYYMMDD", "gameId"]
+    },
+    "PLAYER_GAME_LOG":{
+        "function":"player_game_log",
+        "endpoint":DATA_URL+"/%s/players/%s_gamelog.json",
+        "params":["year", "playerId"]
+    },
+    "PLAYER_UBER_STATS":{
+        "function":"player_uber_stats",
+        "endpoint":DATA_URL+"/%s/players/%_uber_stats.json",
+        "params":["year", "playerId"]
+    },
+    "TEAM_SCHEDULE":{
+        "function":"team_schedule",
+        "endpoint":DATA_URL+"/%s/teams/%s/schedule.json",
+        "params":["year", "teamName"]
+    },
+    "TEAM_ROSTER":{
+        "function":"team_roster",
+        "endpoint":DATA_URL+"/%s/teams/%s/roster.json",
+        "params":["year", "teamName"]
     }
 }
--- nba.py
+++ nba.py
@@ -2,13 +2,26 @@ from methods import methods as m
 from constants import BASE_DATA_URL as base
 import json
 import urllib.request
-import pprint
-
 
 def get_data(url):
     r = urllib.request.urlopen(url)
     data =  json.loads(r.read().decode(r.info().get_param('charset') or 'utf-8'))
     return data
+
+def print_method_list():
+    for key in m:
+        print(m.get(key).get('function'))
+
+def get_method_list():
+    methods = []
+    for key in m:
+        methods.append(m.get(key).get('function'))
+    return methods
+
+def get_params(function):
+    for key in m:
+        if m.get(key).get('function') == function:
+            return m.get(key).get('params')
 
 def nba_data(function, *args):
     func_obj = ""
