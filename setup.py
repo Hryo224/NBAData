da79d9b6bdab74840311d9fccfed2976c038dcbd
Hryo224 <hampton.young@uky.edu>
2017-03-18 13:31:30 -0400

    Uploading Package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'NBAData',
+  packages = ['NBAData'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Wrapper for the data.nba.net API',
+  author = 'Hampton Young',
+  author_email = 'hryoung1994@gmail.com',
+  url = 'https://github.com/hryo224/NBAData', # use the URL to the github repo
+  download_url = 'https://github.com/hryo224/NBAData/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['NBA API', 'NBA Data', 'NBA'], # arbitrary keywords
+  classifiers = [],
+)
